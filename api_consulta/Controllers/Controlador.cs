e34c0dd5ae87de0b978be03fe4daa4615423bd5d
victor49 <vl491249@gmail.com>
2023-05-11 10:59:57 -0500

    se creo un nuevo controlador

---
+++ api_consulta/Controllers/Controlador.cs
@@ -0,0 +1,18 @@
+﻿using api_consulta.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_consulta.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class Controlador : ControllerBase
+    {
+        [HttpGet(Name = "GetMensaje")]
+            
+        public async Task<ActionResult<string>> Get()
+        {
+            string mensaje = "hola a todos";
+            return Ok(mensaje);            
+        }
+    }
+}
